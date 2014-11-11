5d6df42bbb1d8d56e73d96ba764bb3d7af9e23ac
sarianfa <sarianfa@supergator.cisco.com>
2014-11-11 10:43:54 -0500

    only PING flag is set

--- of1.3/virtualizer.py
+++ of1.3/virtualizer.py
@@ -584,10 +584,10 @@ class TheServer:
  
 if __name__ == '__main__':
         server = TheServer('', 9050)
-        LLDP_TEST = 1
-        PING_TEST =0
+        LLDP_TEST = 0
+        PING_TEST =1
         try:
-           if PING_TEST == 1 or LLDP_TEST == 1:
+           if PING_TEST == 1:
            #for this to work it is important that only one ovs switch is active per each physical switch 
             for x in range(2, 5):
                 print x
